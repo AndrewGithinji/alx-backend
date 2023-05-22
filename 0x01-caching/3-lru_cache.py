b6ad47cad6406a843d1e04450b6745d609404a52
AndrewGithinji <andrewgithinji@hotmail.com>
2023-05-22 13:31:39 +0300

    LRU Caching(fix)

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-LRUCache System model
+LRUCache  System model
 """
 from base_caching import BaseCaching
 
@@ -12,31 +12,32 @@ class LRUCache(BaseCaching):
 
     def __init__(self):
         """
-        Initialize the cache
+        Initialize Cache
         """
         super().__init__()
         self.used_key = []
 
     def put(self, key, item):
         """
-        Assign a key-value pair to the cache if the limit is not reached.
-        Otherwise, delete the least recently used item and update the used key.
+        assign a key to value to cache if limit not reached
+        otherwise delete least used item and update used key
         """
-        if key is not None and item is not None:
+        if key and item:
             if len(self.used_key) >= self.MAX_ITEMS:
                 if key not in self.used_key:
-                    del_key = self.used_key.pop()
-                    del self.cache_data[del_key]
-            self.used_key = [key] + [k for k in self.used_key if k != key]
+                    print(f"DISCARD: {self.used_key[-1]}")
+                    del self.cache_data[self.used_key[-1]]
+                del self.used_key[-1]
+            self.used_key = [key] + self.used_key
             self.cache_data[key] = item
 
     def get(self, key):
         """
-        Get the value associated with the given key from the cache.
-        If the key does not exist, return None.
-        Update the used key to reflect the most recently used item.
+            get item from cache if it exists other none
+            update used key
         """
         if key not in self.used_key:
             return None
-        self.used_key = [key] + [k for k in self.used_key if k != key]
-        return self.cache_data.get(key, None)
+        del self.used_key[self.used_key.index(key)]
+        self.used_key = [key] + self.used_key
+        return self.cache_data[key]
